5b35a86839cef1e313257bef23d922be4e2c3015
twilightspike <twilightspike@users.noreply.github.com>
2017-06-28 17:21:24 +0800

    Update 20170628.cpp

--- main/20170628.cpp
+++ main/20170628.cpp
@@ -1,5 +1,8 @@
 int led1 = 13;
 int led2 = 10;
+/*I add the brightness below!*/
+int brightness1 = 87;
+int brightness2 = 209;
 
 void setup(){
   /*to initialize* the digital pin as an output*/
@@ -8,10 +11,14 @@ void setup(){
 }
 
 void loop(){
+  analogWrite(led, brightness);
+  /*
   digitalWrite(led1, HIGH); /*set the LED on*/
   digitalWrite(led2, LOW);
   delay(750);
   digitalWrite(led1, LOW); /*sest the LED off*/
   digitalWrite(led2, HIGH);
   delay(1500); /*wait for 1.5 second*/
+  */
+    
 }
