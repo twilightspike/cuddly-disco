17dc21ac55845b85a806fdb4da65e79031016cba
twilightspike <twilightspike@users.noreply.github.com>
2017-06-28 16:25:08 +0800

    Update 20170628.cpp

--- main/20170628.cpp
+++ main/20170628.cpp
@@ -1,11 +1,11 @@
 void setup(){
-  /*to initialize*/
-  pinMode();
+  /*to initialize* the digital pin as an output*/
+  pinMode(13, OUTPUT); /*Pin 13 has an LED connected*/
 }
 
 void loop(){
-  digitalWrite();
+  digitalWrite(13, HIGH); /*set the LED on*/
   delay(750);
-  digitalWrite();
+  digitalWrite(13, LOW); /*sest the LED off*/
   delay(1500); /*wait for 1.5 second*/
 }
