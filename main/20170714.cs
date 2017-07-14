aedaf363c4387bca545dd2269b7df2481329241c
twilightspike <twilightspike@users.noreply.github.com>
2017-07-14 18:01:36 +0800

    Update 20170714.cs

--- main/20170714.cs
+++ main/20170714.cs
@@ -1,6 +1,13 @@
+#include <cmath>
+
 public struct AABB(Vector2 center, Vector2 halfSize){
   /*public Vector2 center;
   public Vector2 halfSize;*/
   this.center = center;
   this.halfSize = halfSize;
+}
+public bool Ovverlaps(AABB other){
+  if( Math.Abs(center.x - other.center.x) > halfSize.x + other.halfSize.x ) return false;
+  if( Math.Abs(center.y - other.center.y) > halfSize.y + other.halfSize.y ) return false;
+  return true;
 }
