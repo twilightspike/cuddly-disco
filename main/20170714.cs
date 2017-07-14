619f3acbda4bccb888f50e2be6138238b49cdfaa
twilightspike <twilightspike@users.noreply.github.com>
2017-07-14 17:26:25 +0800

    Update 20170714.cs

--- main/20170714.cs
+++ main/20170714.cs
@@ -1,4 +1,6 @@
 public struct AABB(Vector2 center, Vector2 halfSize){
-  public Vector2 center;
-  public Vector2 halfSize;
+  /*public Vector2 center;
+  public Vector2 halfSize;*/
+  this.center = center;
+  this.halfSize = halfSize;
 }
